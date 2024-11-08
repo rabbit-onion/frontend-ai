3fdd4040e68f40f9e749239000cbee4652a03b51
Onion <csr1000@naver.com>
2024-11-08 17:35:47 +0900

    node.js

---
+++ 05-node/main.js
@@ -0,0 +1,7 @@
+import hello, { background as bg, str, str2 } from "./module";
+// import * as abc from "./module";
+
+hello();
+str();
+str2();
+bg();
---
+++ 05-node/module.js
@@ -0,0 +1,21 @@
+function hello() {
+  console.log("hello");
+}
+
+export function str() {
+  console.log("str");
+}
+
+export function str2() {
+  console.log("str2");
+}
+
+export function background() {
+  console.log("background");
+}
+
+export default hello;
+
+// export default function hello() {
+//   console.log("hello");
+// }
